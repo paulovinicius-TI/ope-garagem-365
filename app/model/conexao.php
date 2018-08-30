f7523c1580f74e8ae93a4d7617008a88d5988e7c
alvesmatheus103 <alvesmatheus103@gmail.com>
2018-08-30 09:32:44 -0300

    Update conexao.php

--- app/model/conexao.php
+++ app/model/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "u928575897_serv";
-$pass = "";
+$pass = "projetoope";
 $banco = "u928575897_garag";
 $conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error());
 mysqli_select_db($conexao, $banco) or die(mysqli_error());
